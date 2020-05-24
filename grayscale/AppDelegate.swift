7a8982dad7c7a871912feac761bf8e3daf1482c5
Brett Gutstein <brett.gutstein@cst.cam.ac.uk>
2020-05-24 10:14:25 -0500

    Initial Commit

---
+++ grayscale/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  grayscale
+//
+//  Created by Brett Gutstein on 5/24/20.
+//  Copyright © 2020 Brett Gutstein. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
